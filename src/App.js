cebab929bc017102a1a618f10ab735776a0205bd
unknown <hristinamondovska@gmail.com>
2021-02-09 10:46:43 +0100

    trivia-game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import Level from './components/level/Level';
+import Question from './components/question/Question';
+import './style/css/main.css';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div className="trivia-time">
+          <Switch>
+            <Route path="/" exact component={Level} />
+            <Route exact path="/play" component={Question} />
+            <Route render={
+              () => <h1> 404 Not found</h1>} />
+
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
